ee58dd265b2751f4aa474f6e9bd0d5eae47dd5b1
Izmaelhakki <100159172+Izmaelhakki@users.noreply.github.com>
2023-01-27 10:46:19 +0300

    First Init

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <div>
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 active:bg-blue-800">
+          Buton
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
