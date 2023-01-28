16c595a45c489d9e670488a48d1a109f134531cb
Izmaelhakki <100159172+Izmaelhakki@users.noreply.github.com>
2023-01-28 15:06:01 +0300

    Movies News Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import MoviesSection from "./components/MoviesSection";
 
 function App() {
   return (
     <div>
       <Header />
       <HeroSection/>
+      <MoviesSection/>
     </div>
   );
 }
---
+++ src/components/MoviesSection.jsx
@@ -0,0 +1,318 @@
+import React from "react";
+
+function MoviesSection() {
+  return (
+    <div>
+      {/* Movies Section */}
+      <div className="py-24 bg-black">
+        {/* Movies Content */}
+        <div className="container flex space-x-16">
+          {/* Left Content */}
+          <div className="basis-2/3">
+            {/* Title */}
+            <div className="flex space-x-2 divide-x divide-gega-red divide-opacity-50 mb-8">
+              <h2 className="text-gega-red ">
+                <a href="#">Latest</a>
+              </h2>
+              <h2 className="text-gega-grey pl-2 hover:text-gega-red duration-500">
+                <a href="#">Popular</a>
+              </h2>
+              <h2 className="text-gega-grey pl-2 hover:text-gega-red duration-500">
+                <a href="#">Best</a>
+              </h2>
+            </div>
+            {/* İmages Container */}
+            <div className="flex flex-wrap">
+              {/* İmage 1 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="../../public/images/mv1.jpg"
+                  className="group-hover:scale-110 group-hover:opacity-50 duration-500"
+                />
+                {/* İmage 1 Detail Container */}
+                <div className="absolute px-6 bottom-8 ">
+                  <h3 className="text-gega-grey group-hover:text-gega-melon group-hover:mb-2 duration-500">
+                    Oblivion
+                  </h3>
+                  <p className="text-xs opacity-0 group-hover:opacity-100 group-hover:mb-10 duration-500 text-gega-grey">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  {/* İmage 1 İcons Container */}
+                  <div className="absolute flex space-x-8 text-gega-grey opacity-0 -bottom-2 group-hover:bottom-2 group-hover:opacity-100 duration-500">
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-play"></i>
+                    </a>
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* İmage 2 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="../../public/images/mv2.jpg"
+                  className="group-hover:scale-110 group-hover:opacity-50 duration-500"
+                />
+                {/* İmage 2 Detail Container */}
+                <div className="absolute px-6 bottom-8 ">
+                  <h3 className="text-gega-grey group-hover:text-gega-melon group-hover:mb-2 duration-500">
+                    Oblivion
+                  </h3>
+                  <p className="text-xs opacity-0 group-hover:opacity-100 group-hover:mb-10 duration-500 text-gega-grey">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  {/* İmage 2 İcons Container */}
+                  <div className="absolute flex space-x-8 text-gega-grey opacity-0 -bottom-2 group-hover:bottom-2 group-hover:opacity-100 duration-500">
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-play"></i>
+                    </a>
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* İmage 3 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="../../public/images/mv3.jpg"
+                  className="group-hover:scale-110 group-hover:opacity-50 duration-500"
+                />
+                {/* İmage 3 Detail Container */}
+                <div className="absolute px-6 bottom-8 ">
+                  <h3 className="text-gega-grey group-hover:text-gega-melon group-hover:mb-2 duration-500">
+                    Oblivion
+                  </h3>
+                  <p className="text-xs opacity-0 group-hover:opacity-100 group-hover:mb-10 duration-500 text-gega-grey">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  {/* İmage 3 İcons Container */}
+                  <div className="absolute flex space-x-8 text-gega-grey opacity-0 -bottom-2 group-hover:bottom-2 group-hover:opacity-100 duration-500">
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-play"></i>
+                    </a>
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* İmage 4 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="../../public/images/mv4.jpg"
+                  className="group-hover:scale-110 group-hover:opacity-50 duration-500"
+                />
+                {/* İmage 4 Detail Container */}
+                <div className="absolute px-6 bottom-8 ">
+                  <h3 className="text-gega-grey group-hover:text-gega-melon group-hover:mb-2 duration-500">
+                    Oblivion
+                  </h3>
+                  <p className="text-xs opacity-0 group-hover:opacity-100 group-hover:mb-10 duration-500 text-gega-grey">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  {/* İmage 4 İcons Container */}
+                  <div className="absolute flex space-x-8 text-gega-grey opacity-0 -bottom-2 group-hover:bottom-2 group-hover:opacity-100 duration-500">
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-play"></i>
+                    </a>
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* İmage 5 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="../../public/images/mv5.jpg"
+                  className="group-hover:scale-110 group-hover:opacity-50 duration-500"
+                />
+                {/* İmage 5 Detail Container */}
+                <div className="absolute px-6 bottom-8 ">
+                  <h3 className="text-gega-grey group-hover:text-gega-melon group-hover:mb-2 duration-500">
+                    Oblivion
+                  </h3>
+                  <p className="text-xs opacity-0 group-hover:opacity-100 group-hover:mb-10 duration-500 text-gega-grey">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  {/* İmage 5 İcons Container */}
+                  <div className="absolute flex space-x-8 text-gega-grey opacity-0 -bottom-2 group-hover:bottom-2 group-hover:opacity-100 duration-500">
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-play"></i>
+                    </a>
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* İmage 6 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="../../public/images/mv6.jpg"
+                  className="group-hover:scale-110 group-hover:opacity-50 duration-500"
+                />
+                {/* İmage 6 Detail Container */}
+                <div className="absolute px-6 bottom-8 ">
+                  <h3 className="text-gega-grey group-hover:text-gega-melon group-hover:mb-2 duration-500">
+                    Oblivion
+                  </h3>
+                  <p className="text-xs opacity-0 group-hover:opacity-100 group-hover:mb-10 duration-500 text-gega-grey">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  {/* İmage 6 İcons Container */}
+                  <div className="absolute flex space-x-8 text-gega-grey opacity-0 -bottom-2 group-hover:bottom-2 group-hover:opacity-100 duration-500">
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-play"></i>
+                    </a>
+                    <a
+                      className="hover:text-gega-red"
+                      href="#">
+                      <i className="fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Right Content */}
+          <div className="basis-1/3">
+            {/* Title */}
+            <div className="text-gega-grey mb-8">Hot News</div>
+            {/* News Container */}
+            <div className="flex flex-col h-full pb-16 justify-between">
+              {/*İtems*/}
+              <div className="flex items-center group ">
+                {/*News Image Container*/}
+                <div className="basis-1/3 h-full">
+                  <img
+                    src="../../public/images/new1.jpg"
+                    className="h-full w-full object-cover"
+                    alt=""
+                  />
+                </div>
+                {/*News Detail*/}
+                <div className="pl-4 basis-2/3 text-gega-grey group-hover:text-gega-melon group-hover:cursor-pointer duration-500 ">
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  <p className="text-xs tracking-tighter mt-2 font-bold font-gemunu">On NOW 01 2022</p>
+                </div>
+              </div>
+              {/*İtems*/}
+              <div className="flex items-center group ">
+                {/*News Image Container*/}
+                <div className="basis-1/3 h-full">
+                  <img
+                    src="../../public/images/new2.jpg"
+                    className="h-full w-full object-cover"
+                    alt=""
+                  />
+                </div>
+                {/*News Detail*/}
+                <div className="pl-4 basis-2/3 text-gega-grey group-hover:text-gega-melon group-hover:cursor-pointer duration-500 ">
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  <p className="text-xs tracking-tighter mt-2 font-bold font-gemunu">On NOW 01 2022</p>
+                </div>
+              </div>
+              {/*İtems*/}
+              <div className="flex items-center group ">
+                {/*News Image Container*/}
+                <div className="basis-1/3 h-full">
+                  <img
+                    src="../../public/images/new3.jpg"
+                    className="h-full w-full object-cover"
+                    alt=""
+                  />
+                </div>
+                {/*News Detail*/}
+                <div className="pl-4 basis-2/3 text-gega-grey group-hover:text-gega-melon group-hover:cursor-pointer duration-500 ">
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  <p className="text-xs tracking-tighter mt-2 font-bold font-gemunu">On NOW 01 2022</p>
+                </div>
+              </div>
+              {/*İtems*/}
+              <div className="flex items-center group ">
+                {/*News Image Container*/}
+                <div className="basis-1/3 h-full">
+                  <img
+                    src="../../public/images/new4.jpg"
+                    className="h-full w-full object-cover"
+                    alt=""
+                  />
+                </div>
+                {/*News Detail*/}
+                <div className="pl-4 basis-2/3 text-gega-grey group-hover:text-gega-melon group-hover:cursor-pointer duration-500 ">
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  <p className="text-xs tracking-tighter mt-2 font-bold font-gemunu">On NOW 01 2022</p>
+                </div>
+              </div>
+              {/*İtems*/}
+              <div className="flex items-center group ">
+                {/*News Image Container*/}
+                <div className="basis-1/3 h-full">
+                  <img
+                    src="../../public/images/new5.jpg"
+                    className="h-full w-full object-cover"
+                    alt=""
+                  />
+                </div>
+                {/*News Detail*/}
+                <div className="pl-4 basis-2/3 text-gega-grey group-hover:text-gega-melon group-hover:cursor-pointer duration-500 ">
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sapiente quas
+                  </p>
+                  <p className="text-xs tracking-tighter mt-2 font-bold font-gemunu">On NOW 01 2022</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MoviesSection;
